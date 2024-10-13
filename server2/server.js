1fc72c1c22171cabaa1ddb1b653c3bd74b1ecab7
Brian Mak <brianmak.blue@gmail.com>
2024-10-13 12:30:19 -0700

    changes

--- server2/server.js
+++ server2/server.js
@@ -4,7 +4,7 @@ const url = require('url');
 
 // MySQL connection setup (modify credentials accordingly)
 const connection = mysql.createConnection({
-    host: process.env.DB_HOST || 'localhost',
+    host: process.env.DB_HOST || 'db',
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || 'my-secret-pw',
     database: process.env.DB_DATABASE || 'hospital',
